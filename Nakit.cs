a20af419ec9472b07859e70828f2e8c56421c8c2
SafakOzden <122045260+SafakOzden@users.noreply.github.com>
2023-01-05 21:35:14 +0300

    Create Nakit.cs

---
+++ Nakit.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proje2
+{
+    public class Nakit:Odeme
+    {
+        public int OdenecekUcret { get; set; }
+       public override  void OdemeYap() {
+           
+
+        }
+        
+    }
+}
